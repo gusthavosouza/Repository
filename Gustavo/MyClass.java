ca5803d499bd2d691409495219223fc9098f61a7
Gustavo De Souza <gusthavo.souzapereira@gmail.com>
2014-08-19 14:22:50 -0300

    Update MyClass.java

--- Gustavo/MyClass.java
+++ Gustavo/MyClass.java
@@ -1,8 +1,8 @@
 public MyClass {
 
-public static void main(String args[]){
+    public static void main(String args[]){
 
-System.out.println("Hello Word");
-}
-
-}
+        System.out.println("Hello Word");
+        
+       }
+    }
