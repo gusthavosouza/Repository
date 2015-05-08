48e38c55551b110ced1ba2bedac203aa0b066544
Gustavo De Souza <gusthavo.souzapereira@gmail.com>
2015-05-07 21:53:52 -0300

    Update ThreadCalculate.java

--- Gustavo/ThreadCalculate.java
+++ Gustavo/ThreadCalculate.java
@@ -8,20 +8,22 @@ public class ThreadCalculate implements Runnable {
 	// após receber os dados notifica a Thread que os dados estão prontos para serem calculados exibidos
 	public synchronized void setValues(int a, int b) {
 
-		this.a = a;
-		this.b = b;
-		notify();
+		this.a = a; // guarda o valor recebido do metodo em a
+		this.b = b; // guarda o valor recebido do metodo em b
+		notify(); // notifica a thread para que a mesma saia do estado wait
 	}
 
+	//metodo run que e chamado apos o start da Thread
 	@Override
 	public void run() {
 
 			System.out.println("[ThreadCalculate] Digite 2 numeros para calcular");
 			
-			// apos iniciar a Thread coloca a mesma em estado de espera aguardando a entrada de dados.
 			synchronized (this) {
 				
 				try {
+			System.out.println("Entrando em estado de espera dos dados");
+			// apos iniciar a Thread coloca a mesma em estado de espera aguardando a entrada de dados.	
 					wait();
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,7 +33,7 @@ public class ThreadCalculate implements Runnable {
 
 			System.out.println("Numeros digitados:" + a + "," + b);
 			System.out.println("Soma dos numeros digitados =" + (a + b));
-		System.out.println("Saindo da thread");
+			System.out.println("Saindo da thread");
 	}
 	
 	public synchronized void destroy () {
